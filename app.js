0f46624fb1c07653f606dd9f118e7078d9ba35f9
Muhammad Muzammil <azamcnn@gmail.com>
2023-07-27 07:55:00 +0500

    final 10

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const upload = multer({ dest: 'upload/' })
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfghjkwertyuiodfghjkertyui456d5444sdfgnghjkfeteturiy45dgdty45rtdtddt'
 
-app.use(cors({ credentials: true, origin: 'https://blog-pk2.netlify.app' }))
+app.use(cors({ credentials: true, origin: 'http://www.ebrandworks.com' }))
 app.use(express.json())
 app.use(cookieParser());
 app.use('/upload', express.static(__dirname + '/upload'));
