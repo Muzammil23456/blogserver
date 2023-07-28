21e1aec19051e376496c06acdc7076120d0925ec
Muhammad Muzammil <azamcnn@gmail.com>
2023-07-29 01:12:34 +0500

    test 16

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const fs = require('fs')
 const upload = multer({ dest: 'upload/' })
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfghjkwertyuiodfghjkertyui456d5444sdfgnghjkfeteturiy45dgdty45rtdtddt'
-const port = process.env.PORT || 4000;
+const port = 3001
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.json())
@@ -151,7 +151,7 @@ app.get('*', (req, res, next) => {
     })
 })
 
-// app.listen(port, () => console.log(`port ${port}`));
+app.listen(port, () => console.log(`port ${port}`));
 
 
 // i18kUH1MF3Hl2wOX
