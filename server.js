e8cf2e019eeecee88463adb5f19c7928262f980e
Muhammad Muzammil <azamcnn@gmail.com>
2023-07-28 20:02:48 +0500

    test 0

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-// const port = process.env.PORT || 3000;
-
-
+const port = process.env.PORT || 3000;
 const http = require('http');
 const app = require('./app');
 
 
 const server = http.createServer(app);
-server.listen(3001, () => console.log(`port ${3001}`));
+server.listen(port, () => console.log(`port ${port}`));
