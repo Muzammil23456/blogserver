e0ea4100eb227132cd5bbcf37b2eaccf84457677
Muhammad Muzammil <azamcnn@gmail.com>
2023-07-30 19:05:56 +0500

    just

--- app.js
+++ app.js
@@ -67,25 +67,25 @@ app.post('/logout', (req, res) => {
 })
 
 app.post('/post', async (req, res) => {
-    const { originalname, path } = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length - 1];
-    const newPath = path + '.' + ext;
-    fs.renameSync(path, newPath);
+    // const { originalname, path } = req.file;
+    // const parts = originalname.split('.');
+    // const ext = parts[parts.length - 1];
+    // const newPath = path + '.' + ext;
+    // fs.renameSync(path, newPath);
 
-    const { token } = req.cookies;
-    jwt.verify(token, secret, {}, async (err, info) => {
-        if (err) throw err;
-        const { title, summary, content } = req.body;
-        const postDoc = await Post.create({
-            title,
-            summary,
-            content,
-            cover: newPath,
-            author: info.id,
-        })
-        res.json(postDoc);
-    });
+    // const { token } = req.cookies;
+    // jwt.verify(token, secret, {}, async (err, info) => {
+    //     if (err) throw err;
+    //     const { title, summary, content } = req.body;
+    //     const postDoc = await Post.create({
+    //         title,
+    //         summary,
+    //         content,
+    //         cover: newPath,
+    //         author: info.id,
+    //     })
+    //     res.json(postDoc);
+    // });
     res.json(req.body)
 })
 
