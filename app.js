c9de02c1519c9a4870b82e0eecef39b7a75adc10
Muhammad Muzammil <azamcnn@gmail.com>
2023-07-30 20:50:31 +0500

    just

--- app.js
+++ app.js
@@ -67,6 +67,15 @@ app.post('/logout', (req, res) => {
 })
 
 app.post('/post', upload.single('file'), async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     const { originalname, path } = req.file;
     const parts = originalname.split('.');
     const ext = parts[parts.length - 1];
