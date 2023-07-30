e84654f1e162b9c2cc317c471142943b3f7d4a5e
Muhammad Muzammil <azamcnn@gmail.com>
2023-07-30 23:06:24 +0500

    just

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.post('/post', upload.single('file'), async (req, res) => {
     //     })
     //     res.json(postDoc);
     // });
-    res.json(req.body.file)
+    res.json(req.body)
 })
 
 app.get('/posts', async (req, res) => {
