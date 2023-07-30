fda0f1fb3ebcc113c04a23821b76fd0ee4575e51
Muhammad Muzammil <azamcnn@gmail.com>
2023-07-30 19:48:10 +0500

    just

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post('/logout', (req, res) => {
     res.cookie("token", '',{sameSite: "none", secure:"true" }).json('ok')
 })
 
-app.post('/post', async (req, res) => {
+app.post('/post', upload.single('file') , async (req, res) => {
     const { originalname, path } = req.file;
     const parts = originalname.split('.');
     const ext = parts[parts.length - 1];
